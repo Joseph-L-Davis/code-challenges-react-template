023ecc7b5ebf7cca972d731fc31d3cb4f812a281
Joseph Davis <joethebartenderpdx@gmail.com>
2021-05-09 18:31:29 -0700

    only two passing

--- src/array-methods.js
+++ src/array-methods.js
@@ -13,9 +13,18 @@ export function wordsToCharList(arr) {
 export function listFoods(obj) {
   let arr = [];
   for (let i = 0; i <= obj.length; i ++) {
-    if (i === 'Gruffalo' || 'oats' || 'brown sugar' || 'flour' || 'pur maple syrup' || 'chopped nuts' || 'baking soda' || 'baking powder' || 'cinnamon' || 'melted butter' || 'fresh water') {
+    if (obj[i] === 'Gruffalo' || 'oats' || 'brown sugar' || 'flour' || 'pur maple syrup' || 'chopped nuts' || 'baking soda' || 'baking powder' || 'cinnamon' || 'melted butter' || 'fresh water') {
       arr.push(i);
     } 
   }
   return arr;
+}
+
+export function stepActions(recipe) {
+  let arr = recipe.steps;
+  let arr2 = [];
+  for (let str of arr) {
+    arr2.push(str[0]);
+  }
+  return arr2;
 }
