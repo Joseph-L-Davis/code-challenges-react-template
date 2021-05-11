e1af1b46ef358e1f93c7535117dca8f876419085
Joseph Davis <joethebartenderpdx@gmail.com>
2021-05-10 17:56:33 -0700

    two tests pass

--- src/app/code-challenge-11.js
+++ src/app/code-challenge-11.js
@@ -2,3 +2,13 @@ export function returnTen(str) {
   let newStr = str.slice(-1 - 9);
   return Array.from(newStr);
 }
+
+export function findMax(matrix) {
+  let num = [];
+  for (let i = 0; i < matrix.length; i++) {
+    num[i] = matrix[i].sort((a, b) => b - a)[0];
+  }
+  for (let j = 0; j < num.length; j++){
+    return num.sort((a, b) => b - a)[0];
+  }
+}
--- src/app/code-challenge-11.test.js
+++ src/app/code-challenge-11.test.js
@@ -1,6 +1,12 @@
-import { returnTen } from './code-challenge-11';
+import { findMax, returnTen } from './code-challenge-11';
 
 test('it should return the last 10 characters of a string as an array', () => {
   expect(returnTen('hello world')).toStrictEqual(['e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']);
   expect(returnTen('world')).toStrictEqual(['w', 'o', 'r', 'l', 'd']);
+});
+
+test('return num with highest val', () => {
+  expect(findMax([[1, 3, 4, 5], [4, 5, 6], [23, 5, 5]]
+
+  )).toStrictEqual(23);
 });
